b36f4a05a8aa87e55f8afb1f331e1a12e9704fc0
Dhruv Sharma <sharmdhruv1234@gmail.com>
2024-09-27 21:44:20 +0530

    final

--- app.js
+++ app.js
@@ -13,10 +13,18 @@ const app = express();
 config ({path : "./config/.env"})
 
 app.use(cors({
-    origin:[ process.env.FRONTEND_URL, process.env.DASHBOARD_URL ],
+    origin:[ process.env.FRONTEND_URL, process.env.DASHBOARD_URL, "http://localhost:5173/" ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }))
+
+// app.use(cors({
+//         origin: "http://localhost:3000", // Directly allowing frontend URL
+//         methods: ["GET", "POST", "PUT", "DELETE"],
+//         credentials: true,
+//     }));
+    
+
 
 app.use (cookieParser())
 app.use (express.json())
